0c83d9600e2a37edadc7fd789a52642439ae4888
Alex Krupa <alexk6@outlook.com>
2016-03-09 15:33:29 +0100

    Initial commit.

---
+++ WCFServiceHost/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+using WCFServiceContract;
+
+namespace WCFServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:10002/BaseName/");
+            ServiceHost host = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+                host.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(smb);
+
+                host.Open();
+                Console.WriteLine("Service is running.");
+                Console.WriteLine("Press <ENTER> to quit.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                host.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("Exception: {0}", ce.Message);
+                host.Abort();
+            }
+
+        }
+    }
+}
